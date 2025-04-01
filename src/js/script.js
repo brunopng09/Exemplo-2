ce6cc6f01559841940955ec40675d4f20dae4936
Bruno <brunocostascuciato009@gmail.com>
2025-04-01 09:10:04 -0300

    Declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// DECLARAÇÕES E VARIAVEIS
+var nome = "fiap";
+console.log(nome)
+
+let idade = 17;
+console.log(idade)
+
+const sobrenome = "Scuciato";
+console.log(sobrenome)
+// undefined
+let aula;
+console.log(aula)
+// null
+let valor = null;
+console.log(valor)
+
+let exemplo1 = {};
+
+let exemplo2 = [];
